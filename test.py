18603c5bbc794a3321ad226e8ec47a2979ecc504
GoalKeeper <machine.learner2002@yandex.ru>
2023-04-24 20:42:52 +0300

    1.0 - Add settings with input/output tests

---
+++ test.py
@@ -0,0 +1,48 @@
+import subprocess
+
+
+def check(need: list, got: list, mode: str) -> bool:
+    """
+    Функция, сравнивающая два подаваемых на вход списка - need и got.
+    Существует несколько специальных режимов:
+
+    - All In Order (AIO) - режим, при котором нужно получить точный конкретный вывод - реализован как сравнение списков;
+
+    - All Without Order (AWO) - режим, при котором нужно получить все результаты, но порядок не важен - реализован как сравнение множеств на списках;
+
+    - One From List - режим, при котором нужно вывести m вариантов из n возможных (причем n > m) - проверка на вхождение элементов списка во множество;
+    """
+
+    if mode == 'AIO':
+        return need == got
+
+    if mode == 'AWO':
+        return set(need) == set(got)
+
+    if mode == 'OFL':
+        for g in got:
+            if g not in need:
+                return False
+        return True
+
+
+def get_tests(name: str, tests: list, mode: str):
+    for test in tests:
+
+        fin = open(test + '.in', 'r')
+        fout = open(test + '.out', 'r')
+
+        p = subprocess.Popen(['python', name], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        p.text_mode = True
+        output, _ = p.communicate(''.join(fin.readlines()).encode())
+
+        need = [''.join(fout.readlines()).encode()]
+        got = [output.strip()]
+
+        need = [line.decode().split() for line in need]
+        got = [line.decode().split() for line in got]
+
+        assert check(need, got, 'AIO'), f'Test {test} failed! Need: {str(*need)}, got: {str(*got)}'
+
+
+get_tests("main.py", ['1'], 'AIO')
